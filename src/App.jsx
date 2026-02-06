ee99888f33bf20a7f1187745e1f0be0c90cfd1cb
AungThuraHein <u6726135@au.edu>
2026-02-06 16:24:00 +0700

    Implement user authentication with login, logout, and profile features

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Items } from './components/item.jsx';
-import { ItemEdit } from './components/itemEdit.jsx';
-import TestApi from './components/TestApi.jsx';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Items } from "./components/item.jsx";
+import { ItemEdit } from "./components/itemEdit.jsx";
+import TestApi from "./components/TestApi.jsx";
+import RequireAuth from "./middleware/RequireAuth";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import { UserProvider } from "./context/UserProvider.jsx"
 
 function App() {
   return (
+    <UserProvider>
       <Routes>
         <Route path="/" element={<Navigate to="/items" replace />} />
         <Route path="/items" element={<Items />} />
         <Route path="/items/:id" element={<ItemEdit />} />
         <Route path="/test_api" element={<TestApi />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/profile"
+          element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/logout"
+          element={
+            <RequireAuth>
+              <Logout />
+            </RequireAuth>
+          }
+        />
       </Routes>
+    </UserProvider>
   );
 }
-
 export default App;
